4d3381f4ff57ca039f821b2c51e7a35efd54c524
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2026-05-29 14:58:51 +0200

    feat(router): implement home handler to render index page

--- internal/router/wiki.go
+++ internal/router/wiki.go
@@ -1,1 +1,10 @@
 package router
+
+import (
+	"Forum/pkg/utils"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	utils.RenderFile("Forum Accueil", utils.Render("index", nil), w)
+}
