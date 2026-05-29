f832ea8b031c6f6fd76be92a55bf4d350a8602b8
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2026-05-29 14:59:00 +0200

    feat(router): add home handler and static file serving

--- internal/router/handler.go
+++ internal/router/handler.go
@@ -1,5 +1,17 @@
 package router
 
-func Start() {
+import (
+	"Forum/pkg/utils"
+	"log"
+	"net/http"
+)
 
+func Start() {
+	http.HandleFunc("/", home)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	utils.Log("Server started at : http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
