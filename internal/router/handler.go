58400f7303cb8b0001338e2ed28ad2d1c938d148
caromain <romain.carrere@ynov.com>
2026-06-11 17:18:11 +0200

    feat: switch http.handle in mux.handle

--- internal/router/handler.go
+++ internal/router/handler.go
@@ -1,20 +1,23 @@
 package router
 
 import (
-	"Forum/internal/config"
-	"Forum/pkg/utils"
-	"fmt"
-	"log"
-	"net/http"
+    "Forum/internal/config"
+    "Forum/pkg/utils"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 func Start() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/demo", demo)
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", home)
+    mux.HandleFunc("/demo", demo)
+    mux.HandleFunc("/forbidden", forbidden)
+    mux.HandleFunc("/server-error", serverError)
 
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+    fs := http.FileServer(http.Dir("static"))
+    mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	utils.Log(fmt.Sprintf("Server started at : http://localhost:%v", config.Config.PORT))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.Config.PORT), nil))
+    utils.Log(fmt.Sprintf("Server started at : http://localhost:%v", config.Config.PORT))
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.Config.PORT), mux))
 }
