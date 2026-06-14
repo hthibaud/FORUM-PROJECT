f24e73319037c45eefe657826b36da3c47bcf6cb
caromain <romain.carrere@ynov.com>
2026-06-14 15:36:34 +0200

    feat: add login page

--- internal/router/handler.go
+++ internal/router/handler.go
@@ -1,24 +1,25 @@
 package router
 
 import (
-    "Forum/internal/config"
-    "Forum/pkg/utils"
-    "fmt"
-    "log"
-    "net/http"
+	"Forum/internal/config"
+	"Forum/pkg/utils"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func Start() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/demo", demo)
-    mux.HandleFunc("/forbidden", forbidden)
-    mux.HandleFunc("/server-error", serverError)
-    mux.HandleFunc("/register", register)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/demo", demo)
+	mux.HandleFunc("/forbidden", forbidden)
+	mux.HandleFunc("/server-error", serverError)
+	mux.HandleFunc("/register", register)
+	mux.HandleFunc("/login", login)
 
-    fs := http.FileServer(http.Dir("static"))
-    mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    utils.Log(fmt.Sprintf("Server started at : http://localhost:%v", config.Config.PORT))
-    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.Config.PORT), mux))
+	utils.Log(fmt.Sprintf("Server started at : http://localhost:%v", config.Config.PORT))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.Config.PORT), mux))
 }
--- internal/router/wiki.go
+++ internal/router/wiki.go
@@ -1,29 +1,33 @@
 package router
 
 import (
-    "Forum/pkg/utils"
-    "net/http"
+	"Forum/pkg/utils"
+	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        utils.RenderError(http.StatusNotFound, "404 - Page introuvable", "Erreur 404", "La page demandée n'existe pas.", w)
-        return
-    }
-    utils.RenderFile("Forum Accueil", utils.Render("index", nil), w)
+	if r.URL.Path != "/" {
+		utils.RenderError(http.StatusNotFound, "404 - Page introuvable", "Erreur 404", "La page demandée n'existe pas.", w)
+		return
+	}
+	utils.RenderFile("Forum Accueil", utils.Render("index", nil), w)
 }
 
 func demo(w http.ResponseWriter, r *http.Request) {
-    utils.RenderFile("Forum Demo", utils.Render("demo", nil), w)
+	utils.RenderFile("Forum Demo", utils.Render("demo", nil), w)
 }
 
 func forbidden(w http.ResponseWriter, r *http.Request) {
-    utils.RenderError(http.StatusForbidden, "403 - Accès refusé", "Erreur 403", "Vous n’avez pas les droits nécessaires pour accéder à cette page.", w)
+	utils.RenderError(http.StatusForbidden, "403 - Accès refusé", "Erreur 403", "Vous n’avez pas les droits nécessaires pour accéder à cette page.", w)
 }
-func register(w http.ResponseWriter, r *http.Request){
+func register(w http.ResponseWriter, r *http.Request) {
 	utils.RenderFile("Forum Register", utils.Render("register", nil), w)
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	utils.RenderFile("Forum Login", utils.Render("login", nil), w)
+}
+
 func serverError(w http.ResponseWriter, r *http.Request) {
-    utils.RenderError(http.StatusInternalServerError, "500 - Erreur serveur", "Erreur 500", "Une erreur interne est survenue. Merci de réessayer plus tard.", w)
+	utils.RenderError(http.StatusInternalServerError, "500 - Erreur serveur", "Erreur 500", "Une erreur interne est survenue. Merci de réessayer plus tard.", w)
 }
